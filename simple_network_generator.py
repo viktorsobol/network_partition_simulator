2f748c2c38993121b494c0bb072ba5656efc0baf
Sobolvitya <viktor.pdt@gmail.com>
2019-06-28 20:32:31 +0300

    add very simple simulation

--- simple_network_generator.py
+++ simple_network_generator.py
@@ -1,5 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
+import time
 
 class NodeRepresantation:
     def colourOf(self, status: str) -> str:
@@ -28,11 +30,28 @@ pos = nx.spring_layout(G)
 
 device_represantation = NodeRepresantation()
 
-edge_colors = ['black' if not edge in [] else 'red' for edge in G.edges()]
-node_colors = [ device_represantation.colourOf(node[1]['status']) for node in G.nodes(data=True)]
 
-nx.draw_networkx_edges(G, pos)
-nx.draw_networkx_nodes(G, pos, node_color=node_colors)
-nx.draw_networkx_labels(G, pos)
+for i in  range(40):
+    
+    print('Starting iteration #' + str(i))
+    # time.sleep(3)
+    
+    for node in G.nodes(data=True):
+        rand = random.randint(0, 1)
+        if rand == 0:
+            node[1]['status'] = 'DOWN'
+        else:
+            node[1]['status'] = 'UP'
+        print(node)
+
+    edge_colors = ['black' if not edge in [] else 'red' for edge in G.edges()]
+    node_colors = [ device_represantation.colourOf(node[1]['status']) for node in G.nodes(data=True)]
+
+    nx.draw_networkx_edges(G, pos)
+    nx.draw_networkx_nodes(G, pos, node_color=node_colors)
+    nx.draw_networkx_labels(G, pos)
+
+    plt.pause(2)
 
 plt.show()
+
