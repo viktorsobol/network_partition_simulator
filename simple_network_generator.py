7c0fb65f819d03e67069a3720e7da74e14e245a3
Sobolvitya <viktor.pdt@gmail.com>
2019-07-29 16:55:27 +0700

    add generation of failure based on poisson distribution

--- simple_network_generator.py
+++ simple_network_generator.py
@@ -1,7 +1,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 import random
 import time
+import uuid
+import constants
 
 class NodeRepresantation:
     def colourOf(self, status: str) -> str:
@@ -12,10 +15,33 @@ class NodeRepresantation:
         raise Exception('No such status')
 
 class NetworkTopologyGenerator:
-
     def get(self) -> nx.Graph:
         return nx.random_regular_graph(4, 10)
 
+class Node:
+    status = 'UP'
+    timLeft = 0
+
+    def __init__ (self):
+        self.timLeft = np.random.poisson(constants.MEAN.get('UP'))
+
+    def tick(self):
+        self.timLeft -= 1
+
+        if self.timLeft == 0:
+            self.statusChange()    
+            self.timLeft = np.random.poisson(constants.MEAN.get(self.status))
+
+    def statusChange(self):
+        if self.status == 'UP':
+            self.status = 'DOWN'
+            return
+        if self.status == 'DOWN':
+            self.status = 'UP'
+            return
+    
+    def __str__(self):
+        return self.status + ':  ' + str(self.timLeft)
 
 G = nx.Graph()
 
@@ -39,32 +65,31 @@ topologyGenerator = NetworkTopologyGenerator()
 G = topologyGenerator.get()
 
 
+for node in G.nodes(data=True):
+    node[1]['metadata'] = Node()
+
 pos = nx.spring_layout(G)
 
 device_represantation = NodeRepresantation()
 
 
-for i in  range(40):
+for i in  range(100):
     
     print('Starting iteration #' + str(i))
-    # time.sleep(3)
     
     for node in G.nodes(data=True):
-        rand = random.randint(0, 1)
-        if rand == 0:
-            node[1]['status'] = 'DOWN'
-        else:
-            node[1]['status'] = 'UP'
+        node[1]['metadata'].tick()
         print(node)
+        print(node[1]['metadata'])
 
     edge_colors = ['black' if not edge in [] else 'red' for edge in G.edges()]
-    node_colors = [ device_represantation.colourOf(node[1]['status']) for node in G.nodes(data=True)]
+    node_colors = [ device_represantation.colourOf(node[1]['metadata'].status) for node in G.nodes(data=True)]
 
     nx.draw_networkx_edges(G, pos)
     nx.draw_networkx_nodes(G, pos, node_color=node_colors)
     nx.draw_networkx_labels(G, pos)
 
-    plt.pause(2)
+    plt.pause(1)
 
 plt.show()
 
