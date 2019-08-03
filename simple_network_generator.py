bc47cae7f5f9d1c750c18a6e050ec8cf297d1ca7
Sobolvitya <viktor.pdt@gmail.com>
2019-08-03 10:47:53 +0700

    .

--- simple_network_generator.py
+++ simple_network_generator.py
@@ -16,7 +16,7 @@ class NodeRepresantation:
 
 class NetworkTopologyGenerator:
     def get(self) -> nx.Graph:
-        return nx.random_regular_graph(4, 10)
+        return nx.random_regular_graph(6, 100)
 
 class Node:
     status = 'UP'
@@ -28,7 +28,7 @@ class Node:
     def tick(self):
         self.timLeft -= 1
 
-        if self.timLeft == 0:
+        if self.timLeft <= 0:
             self.statusChange()    
             self.timLeft = np.random.poisson(constants.MEAN.get(self.status))
 
@@ -43,53 +43,93 @@ class Node:
     def __str__(self):
         return self.status + ':  ' + str(self.timLeft)
 
-G = nx.Graph()
+def split_brain(nodes: list, G) -> bool:
+    if (len(nodes) == 0):
+        return False
+    nodes_up = nodes
+    to_visit = [nodes_up[0]]
+    visited = set()
+   
+    while(len(to_visit) > 0):
+        new_nodes = list(G.adj[to_visit[0]])
+        visited.add(to_visit[0])
+        to_visit.remove(to_visit[0])
+       
+        for new_node in new_nodes:
+            if (G.nodes(data=True)[new_node]['metadata'].status == 'DOWN'):
+                continue
+            if (new_node in visited):
+                continue
+            to_visit.append(new_node)
+    
+    return len(visited) != len(nodes)
 
-G.add_node(1, status='UP')
-G.add_node(2, status='DOWN')
-G.add_node(3, status='UP')
-G.add_node(4, status='UP')
-G.add_node(5, status='UP')
+# G = nx.Graph()
 
-G.add_edge(1, 2)
-G.add_edge(3, 2)
-G.add_edge(4, 2)
-G.add_edge(5, 1)
-G.add_edge(5, 2)
+# G.add_node(1, status='UP')
+# G.add_node(2, status='DOWN')
+# G.add_node(3, status='UP')
+# G.add_node(4, status='UP')
+# G.add_node(5, status='UP')
+
+# G.add_edge(1, 2)
+# G.add_edge(3, 2)
+# G.add_edge(4, 2)
+# G.add_edge(5, 1)
+# G.add_edge(5, 2)
 
 # G1 = nx.complete_graph(10)
 # G1 = nx.star_graph(10)
 # G1 = nx.margulis_gabber_galil_graph(3)
+
+def run_epoch(G) -> int:
+
+
+
+    for node in G.nodes(data=True):
+        node[1]['metadata'] = Node()
+
+    # pos = nx.spring_layout(G)
+
+    # device_represantation = NodeRepresantation()
+
+    total_count_of_failures = 0
+
+    for i in  range(1000):
+        
+        # print('Starting iteration #' + str(i))
+        
+        for node in G.nodes(data=True):
+            node[1]['metadata'].tick()
+            # print(node)
+            # print(node[1]['metadata'])
+
+        up_nodes = [x for x,y in G.nodes(data=True) if y['metadata'].status == 'UP']
+
+        # print(up_nodes)
+        res = split_brain(list(up_nodes), G)
+        if res == True:
+            total_count_of_failures += 1
+        # print("Result is " + str(res))
+
+        # edge_colors = ['black' if not edge in [] else 'red' for edge in G.edges()]
+        # node_colors = [ device_represantation.colourOf(node[1]['metadata'].status) for node in G.nodes(data=True)]
+
+        # nx.draw_networkx_edges(G, pos)
+        # nx.draw_networkx_nodes(G, pos, node_color=node_colors)
+        # nx.draw_networkx_labels(G, pos)
+
+        # plt.pause(0.2)
+    # plt.show()
+    print("Total failures: " + str(total_count_of_failures))
+    return total_count_of_failures
+
+res = 0
 topologyGenerator = NetworkTopologyGenerator()
 
 G = topologyGenerator.get()
 
-
-for node in G.nodes(data=True):
-    node[1]['metadata'] = Node()
-
-pos = nx.spring_layout(G)
-
-device_represantation = NodeRepresantation()
-
-
-for i in  range(100):
-    
-    print('Starting iteration #' + str(i))
-    
-    for node in G.nodes(data=True):
-        node[1]['metadata'].tick()
-        print(node)
-        print(node[1]['metadata'])
-
-    edge_colors = ['black' if not edge in [] else 'red' for edge in G.edges()]
-    node_colors = [ device_represantation.colourOf(node[1]['metadata'].status) for node in G.nodes(data=True)]
-
-    nx.draw_networkx_edges(G, pos)
-    nx.draw_networkx_nodes(G, pos, node_color=node_colors)
-    nx.draw_networkx_labels(G, pos)
-
-    plt.pause(1)
-
-plt.show()
-
+for i in range(0, 100):
+    print("Iteration #" + str(i))
+    res += run_epoch(G)
+print("Average: " + str(res/100))
